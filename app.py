48309009682e626186c17783d46149e13c52b3e0
chris.weber <chris.weber@nerdery.com>
2022-03-02 19:12:31 -0600

    Progress: Module 10: Prepare for challenge.

--- app.py
+++ app.py
@@ -21,3 +21,8 @@ def scrape():
     mars_data = scraping.scrape_all()
     mars.update_one({}, {"$set": mars_data}, upsert=True)
     return redirect('/', code=302)
+
+
+@app.route("/hello")
+def hello():
+    return "Does this work?"
--- scraping.py
+++ scraping.py
@@ -79,7 +79,7 @@ def featured_image(browser):
     img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
 
     return img_url
-1
+
 
 def mars_facts():
     # Add try/except for error handling
